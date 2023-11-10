0792ae30ecec9fd37af24aac4fc487f3d62f8519
Charles F. Munat <charles.munat@gmail.com>
2023-11-10 20:22:31 +1300

    0.0.8

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,4 @@ export const SITE_TITLE = "Jake Baxendale"
 export const SITE_DESCRIPTION = collapseWhitespace(`
 	Jake Baxendale is a jazz musician, composer, and bandleader. He plays saxophone. Jake&apos;s crib is in Wellington, New Zealand.
 `)
-	""
+;("")
