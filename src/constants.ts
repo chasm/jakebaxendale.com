2cd49771c525158013248d8765dadd35f900ad2f
Charles Munat <charles.munat@gmail.com>
2024-02-18 13:48:56 +1300

    0.0.15

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,42 @@
-import { collapseWhitespace } from "@sitebender/components"
+import collapseWhitespace from "~utilities/collapseWhitespace"
 
-export const SITE_TITLE = "Jake Baxendale"
+export const GLOBAL_NAV = [
+	{ href: "/bio", label: "Bio" },
+	{ href: "/community", label: "Community" },
+	{ href: "/discography", label: "Discography" },
+	{ href: "/learning", label: "Lessons" },
+	{ href: "/portfolio", label: "Portfolio" },
+	{ href: "/projects", label: "Projects" },
+	{ href: "/prose", label: "Prose" },
+	{ href: "/contact", label: "Contact" },
+]
+
+export const SITE_AUTHOR = "Jake Baxendale"
+export const SITE_CANONICAL_URL = "https://jakebaxendale.com"
 export const SITE_DESCRIPTION = collapseWhitespace(`
-	Jake Baxendale is a jazz musician, composer, and bandleader. He plays saxophone. Jake&apos;s crib is in Wellington, New Zealand.
+	Jake Baxendale is a jazz musician, composer, and bandleader.
+	He plays saxophone. Jake&apos;s crib is in Wellington, New Zealand.
 `)
-;("")
+export const SITE_KEYWORDS = [
+	"alto saxophone",
+	"Aotearoa",
+	"bandleader",
+	"composer",
+	"Jake Baxendale",
+	"jazz musician",
+	"jazz",
+	"musician",
+	"New Zealand",
+	"saxophone",
+	"Tao",
+	"Wellington",
+]
+export const SITE_PUBLISHER = "Jake Baxendale"
+export const SITE_TITLE = "Jake Baxendale"
+
+export const SUPPLEMENTAL_NAV = [
+	{ href: "/cookie-policy", label: "Cookie policy" },
+	{ href: "/feedback", label: "Feedback" },
+	{ href: "/privacy-policy", label: "Privacy policy" },
+	{ href: "/terms-of-use", label: "Terms of use" },
+]
