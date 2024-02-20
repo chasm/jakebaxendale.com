069a1959b0a6aba3c27b3ef1b5085153974ff4b9
Charles Munat <charles.munat@gmail.com>
2024-02-20 22:33:12 +1300

    0.0.22

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,36 @@
 export type PageMeta = {
 	blurb?: string | undefined
+	image?: Image | undefined
 	label?: string | undefined
+	subtitle?: string | undefined
 	title: string
+}
+
+export type MediaType = "image/avif" | "image/jpeg" | "image/png" | "image/webp"
+
+export type Image = {
+	alt?: string | undefined
+	class?: string | undefined
+	filename: string
+	height: number
+	loading?: "eager" | "lazy" | undefined
+	src: string
+	types: Array<MediaType>
+	width: number
 }
 
 export type Card = {
 	blurb?: string | undefined
-	href?: string
-	src?: string
-	subtitle?: string
+	href?: string | undefined
+	image?: Image | undefined
+	subtitle?: string | undefined
 	title: string
-	year?: number
+	year?: number | undefined
 }
 
 export type Link = {
 	href: string
 	label: string
+	rel?: "external" | undefined
+	title?: string | undefined
 }
